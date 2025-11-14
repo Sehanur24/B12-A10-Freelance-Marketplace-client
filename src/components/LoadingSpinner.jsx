3ca2be93756a6aebc7c6b6dddb7a4cfb9025c20f
sehanur24 <sehanurrahmansiam@gmail.com>
2025-11-14 23:11:47 +0600

    fifth commit

---
+++ src/components/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+    return (
+        <div className="min-h-[60vh] flex items-center justify-center">
+            <div className="loading"></div>
+        </div>
+    );
+};
+
+export default LoadingSpinner;
